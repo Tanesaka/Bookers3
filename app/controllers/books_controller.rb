05a9b208ab4cf5e595554f7838e95194d2c097b4
Tanesaka <seedtanesa@outlook.jp>
2020-11-18 23:44:04 +0000

    Update

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+    redirect_to books_path, notice: 'You have created book successfully.'
+    else
+      @books = Book.all
+      @myuser = current_user
+    render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @myuser = current_user
+    @books = Book.all
+  end
+
+  def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @bookuser = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path, notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path,notice: 'You have deleted book successfully.'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b3a5ca8430ad05313e5994c2082d62e4aa639453814d82d74d7b45f71f6164b8a639376d2a810ed59a1f218095bd68e89c59fd7eeca53740ac79e8e112867639'
